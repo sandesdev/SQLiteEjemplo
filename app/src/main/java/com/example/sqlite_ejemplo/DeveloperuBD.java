3660418dca4c697bad4dfb90f53378ebd036b2db
sandesdev <sgs.desarrollo@gmail.com>
2021-05-19 10:36:13 -0300

    Primera Clase 2

---
+++ app/src/main/java/com/example/sqlite_ejemplo/DeveloperuBD.java
@@ -0,0 +1,40 @@
+package com.example.sqlite_ejemplo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.security.KeyStore;
+
+public class DeveloperuBD extends SQLiteOpenHelper {
+    private static final String Nombre_BD="developerubd.bd";
+    private static final int Version_BD=1;
+    private static final String TABLA_CURSOS="CREATE TABLE CURSOS(CODIGO INTEGER PRIMARY KEY AUTOINCREMENT  ,CURSO TEXT,CARRERA TEXT)";
+
+    public DeveloperuBD(Context context) {
+        super(context, Nombre_BD, null, Version_BD);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(TABLA_CURSOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS '" + TABLA_CURSOS + "'");
+        sqLiteDatabase.execSQL(TABLA_CURSOS);
+    }
+    public void agregarCursos(String codigo,String curso,String carrera)
+    {
+        SQLiteDatabase bd=getWritableDatabase();
+        if(bd!=null)
+        {
+            bd.execSQL("INSERT INTO CURSOS VALUES('"+codigo+"','"+curso+"','"+carrera+"')");
+            bd.close();
+        }
+    }
+}
--- app/src/main/java/com/example/sqlite_ejemplo/MainActivity.java
+++ app/src/main/java/com/example/sqlite_ejemplo/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.sqlite_ejemplo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtCodigo,edtCarrera,edtCurso;
+    Button btnAgregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtCodigo=(EditText)findViewById(R.id.edtCodigo);
+        edtCarrera=(EditText)findViewById(R.id.edtCarrera);
+        edtCurso=(EditText)findViewById(R.id.edtCurso);
+        btnAgregar=(Button)findViewById(R.id.btnAgregar);
+        final  DeveloperuBD developeruBD=new DeveloperuBD(getApplicationContext());
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                developeruBD.agregarCursos(edtCodigo.getText().toString(),edtCurso.getText().toString(),edtCarrera.getText().toString());
+
+                Toast.makeText(getApplicationContext(),"SE AGREGO CORRECTAMENTE",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
+
