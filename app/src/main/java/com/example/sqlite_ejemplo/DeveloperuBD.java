e466eba13a6a6b38b68e9f266a81ac726eab4331
sandesdev <sgs.desarrollo@gmail.com>
2021-05-20 10:45:21 -0300

    1

--- app/src/main/java/com/example/sqlite_ejemplo/DeveloperuBD.java
+++ app/src/main/java/com/example/sqlite_ejemplo/DeveloperuBD.java
@@ -12,7 +12,7 @@ import java.security.KeyStore;
 public class DeveloperuBD extends SQLiteOpenHelper {
     private static final String Nombre_BD="developerubd.bd";
     private static final int Version_BD=1;
-    private static final String TABLA_CURSOS="CREATE TABLE CURSOS(CODIGO INTEGER PRIMARY KEY AUTOINCREMENT  ,CURSO TEXT,CARRERA TEXT)";
+    private static final String TABLA_CURSOS="CREATE TABLE CURSOS(CODIGO INTEGER PRIMARY KEY ,CURSO TEXT,CARRERA TEXT)";
 
     public DeveloperuBD(Context context) {
         super(context, Nombre_BD, null, Version_BD);
