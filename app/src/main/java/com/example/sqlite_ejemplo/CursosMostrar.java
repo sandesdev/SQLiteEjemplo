48668121dd9b0eb5d0a7ffa1f7a651ec095f41a8
sandesdev <sgs.desarrollo@gmail.com>
2021-05-20 09:23:01 -0300

    1

---
+++ app/src/main/java/com/example/sqlite_ejemplo/CursosMostrar.java
@@ -0,0 +1,14 @@
+package com.example.sqlite_ejemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CursosMostrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cursos_mostrar);
+    }
+}
